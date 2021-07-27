0e0871f2c8a1d8b7037c534f31358359c5484309
ronald <ronaldjunior2@hotmail.com>
2021-07-27 16:48:08 -0300

    preparando review

--- src/schemas/songSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from 'joi';
-
-const songSchema = joi.object({
-    name: joi.string().min(3).required(),
-    youtubeLink: joi.string().pattern(new RegExp(/^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/)).required(),
-});
-
-export { songSchema };
--- tests/utils/database.ts
+++
@@ -1,5 +0,0 @@
-import connection from '../../src/database'
-
-export async function clearDatabase(){
-    await connection.query(`TRUNCATE songs RESTART IDENTITY CASCADE`)
-}
